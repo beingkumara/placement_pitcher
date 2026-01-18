fe5c863bb6a68d9910e07333adf1e129377baa88
beingkumara <gutti.teja.20033@iitgoa.ac.in>
2026-01-18 18:08:30 +0530

    fix: force IPv4 stack in BackendApplication.java

--- backend/src/main/java/com/placementpitcher/backend/BackendApplication.java
+++ backend/src/main/java/com/placementpitcher/backend/BackendApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("java.net.preferIPv4Stack", "true");
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
